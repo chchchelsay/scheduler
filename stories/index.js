7334fac5a86ab27d7be72615283601892a1f44d3
Chelsea Dwarika <chelseakristina@live.com>
2023-02-03 15:17:13 -0500

    update Header component to accept props.time instead of hardcoded 12pm

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,7 @@ import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
+import Header from "components/Appointment/Header.js";
 
 //BUTTON STORIES
 storiesOf("Button", module)
@@ -145,3 +146,12 @@ storiesOf("Appointment", module)
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />);
+
+//HEADER STORIES
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />);
