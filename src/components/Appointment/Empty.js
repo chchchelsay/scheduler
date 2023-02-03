e48d263bff6fa284672ff99f683ce7b8c079f199
Chelsea Dwarika <chelseakristina@live.com>
2023-02-03 15:18:39 -0500

    create Empty appt component

--- src/components/Appointment/Empty.js
+++ src/components/Appointment/Empty.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import 'components/Appointment/styles.scss';
 
-export default function Header(props) {
+export default function Empty(props) {
   return (
-    <header className="appointment__time">
-      <h4 className="text--semi-bold">{props.time}</h4>
-      <hr className="appointment__separator" />
-    </header>
-  )
-}
+  <main className="appointment__add">
+  <img
+    className="appointment__add-button"
+    src="images/add.png"
+    alt="Add"
+  />
+</main>  
+  );
+};
