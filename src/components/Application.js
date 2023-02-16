f36a48809af5f8b73ef806ae87dc2b4828729c38
Chelsea Dwarika <chelseakristina@live.com>
2023-02-16 13:32:36 -0500

    implement custom hooks, add tests for first/second/third

--- src/components/Application.js
+++ src/components/Application.js
@@ -31,14 +31,17 @@ export default function Application(props) {
       )
   });
 
- useEffect(() => {
-      Promise.all([
-        axios.get('http://localhost:8001/api/days'),
-        axios.get('http://localhost:8001/api/appointments'),
-        axios.get('http://localhost:8001/api/interviewers')      ]).then((res) => {
-        setState(prev => ({...prev, days: res[0].data, appointments: res[1].data, interviewers: res[2].data}))
-      });
-  }, []);
+useEffect(() => {
+    Promise.all([
+    axios.get('/api/days'),
+    axios.get('/api/appointments'),
+    axios.get('/api/interviewers')
+    ])
+    .then(all => {
+      //console.log('all:',all);
+    setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
+    });
+  }, [])
 
   return (
     <main className="layout">
