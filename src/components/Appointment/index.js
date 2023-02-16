5d9221dd8af7e52ceb9a78b7b1f93952c7e5088a
Chelsea Dwarika <chelseakristina@live.com>
2023-02-16 14:28:49 -0500

    update custom hook in index.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,27 +1,43 @@
 import React from 'react';
-import 'components/Appointment/styles.scss';
-import { useVisualMode } from "hooks/useVisualMode";
+import "components/Appointment/styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
+import Form from './Form';
 
-import Header from './Header';
-import Show from './Show';
-import Empty from './Empty';
-import Form from "./Form";
-
-export default function Appointment(props) {
+export default function Appointment (props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const CONFIRM = "CONFIRM";
+  const EDIT = "EDIT";
+  //const SAVING = "SAVING";
+  //const DELETING = "DELETING";
+  //const ERROR = "ERROR";
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
 
+  function onAdd() {
+    transition(CREATE)
+  }
+  function onCancel() {
+    back()
+  }
+  function onDelete() {
+    transition(CONFIRM)
+  }
+  function onEdit() {
+    transition(EDIT)
+  }
+
   return (
-    <>
-    <article 
-    className="appointment">
-      <Header time={props.time} />
-      {mode === EMPTY && (
+  <>
+  <Header time={props.time} />  
+  <article className="appointment">
+    {mode === EMPTY && (
       <Empty onAdd={onAdd} />
     )}
     {mode === SHOW && (
