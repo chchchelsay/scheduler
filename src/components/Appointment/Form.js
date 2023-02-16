3d26a354b685e166597170caad95395ff1887e49
Chelsea Dwarika <chelseakristina@live.com>
2023-02-16 17:30:41 -0500

    use hook for Cancel

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -3,26 +3,29 @@ import Button from 'components/Button';
 import InterviewerList from 'components/InterviewerList';
 
 export default function Form(props) {
-  const [currentName, setName] = useState(props.name || "");
-  const [currentInterviewer, setInterviewer] = useState(props.value || null)
+  const [currentName, setName] = useState(props.currentName || "");
+  const [currentInterviewer, setInterviewer] = useState(props.interviewer || null)
 
   const reset = () => {
     setName("")
     setInterviewer('null')
   }
 
-  function cancel () {
+  const cancel = function() {
     reset();
-    props.onCancel()
+    props.onCancel();
   }
+
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form 
+        autoComplete="off"
+        onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
-            name={props.name}
+            name="name"
             type="text"        
             onChange={(event) => setName(event.target.value)}            
             value={currentName}
@@ -30,13 +33,16 @@ export default function Form(props) {
             />
 
         </form>
-        <InterviewerList interviewers={props.interviewers} value={currentInterviewer} onChange={(event) => setInterviewer(event)} />
+        <InterviewerList 
+        interviewers={props.interviewers}
+        interviewer={currentInterviewer}
+        onChange={(event) => setInterviewer(event)} 
+        />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onSubmit={event => event.preventDefault()} onClick={event => props.onSave(currentName, currentInterviewer)}>Save</Button>
-        </section>
+        <Button danger onClick={cancel}>Cancel</Button>
+        <Button confirm onClick={() => props.onSave(currentName, currentInterviewer)}>Save</Button>        </section>
       </section>
     </main>
   )
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import 'components/Appointment/styles.scss';
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
+import useVisualMode from "hooks/useVisualMode";
+import Form from './Form';
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
 
   return (
-    <article 
-    className="appointment">
-      <Header time={props.time} />
-      {props.interview ?
+    <Fragment>
+      <Header time={props.time}></Header>
+      {mode === EMPTY && 
+      <Empty onAdd={() => transition(CREATE)} 
+      />}
+      
+      {mode === SHOW && (
         <Show
-        student={props.interview.student}
-        interviewer={[props.interview.interviewer]} />
-      : <Empty />
-      }
-    </article>
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />)}
+
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onCancel={() => transition(EMPTY)}
+        />)}
+    </Fragment>
   )
 }
