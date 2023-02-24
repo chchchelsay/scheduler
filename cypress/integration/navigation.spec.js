bb7375d1b64596ea03390c16daec33c614f80b85
Chelsea Dwarika <chelseakristina@live.com>
2023-02-24 13:27:53 -0500

    run initia Appointments testing with Cypress: Add/Book

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -2,12 +2,16 @@ describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
   });
-  
+
   it("should navigate to Tuesday", () => {
+    // Open the site
     cy.visit("/");
-  
+
+    // Click on Tuesday
+    // Check that the nav item is highlighted
     cy.contains("[data-testid=day]", "Tuesday")
       .click()
-      .should("have.class", "day-list__item--selected")
+      .should("have.class", "day-list__item--selected");
+
   });
 });
